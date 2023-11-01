0e07050bba9075be66d04124a178309c996cb0eb
FranAprigio <francianefad@outlook.com>
2023-11-01 05:19:17 -0300

    [FEAT]: add conteundo em praticas advertidas prof

---
+++ src/pages/profissionalPages/praticasAdvertidas/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+export const Form = styled.View`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 100px;
+  justify-content: space-around;
+  width: 90%;
+
+`;
--- src/pages/profissionalPages/praticasAdvertidas/subpages/ocitocina.js
+++ src/pages/profissionalPages/praticasAdvertidas/subpages/ocitocina.js
@@ -1,10 +1,26 @@
-import { BackgroundComponent } from "../../../../components/micros";
-import { StyleSheet, View } from "react-native";
+import React from 'react';
+import { BackgroundComponent, TextComponent } from "../../../../components/micros/index";
+import * as S from "../styles"
+import { StyleSheet } from "react-native";
 
 export const Ocitocina = () => {
-  <BackgroundComponent colors={["#FCCCD4", "#FCE5BF", "#E3C3B9"]}>
-    <View style={styles.body}></View>
-  </BackgroundComponent>;
+  return (
+    <BackgroundComponent colors={['#FCCCD4', '#FCE5BF', '#E3C3B9']}>
+      <S.Container>
+        <S.Form>
+          <TextComponent variant="Title1">
+
+
+          O lado ruim da ocitocina sintética inclui o risco de contrações uterinas excessivas, distocia,
+           necessidade de monitoramento intensivo, aumento do risco de cesariana e possíveis efeitos colaterais. 
+           No entanto, seu uso é geralmente benéfico quando administrado corretamente em situações clínicas apropriadas. 
+           A decisão de uso deve ser baseada na avaliação da saúde da mãe e do bebê, seguindo diretrizes rigorosas.
+
+          </TextComponent>
+        </S.Form>
+      </S.Container>
+    </BackgroundComponent>
+  );
 };
 
 const styles = StyleSheet.create({
