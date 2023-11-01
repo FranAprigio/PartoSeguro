9a6210cda7192675e8600d1c53fd5da2c0d17dac
FranAprigio <francianefad@outlook.com>
2023-11-01 05:01:41 -0300

    [FEAt]: Estudo de caso cesariana prof

--- src/pages/profissionalPages/estudoCasosProf/subPages/partoDomiciliar.js
+++ src/pages/profissionalPages/estudoCasosProf/subPages/partoDomiciliar.js
@@ -1,10 +1,26 @@
-import { BackgroundComponent } from "../../../../components/micros";
-import { StyleSheet, View } from "react-native";
+import React from 'react';
+import { BackgroundComponent, TextComponent } from "../../../../components/micros/index";
+import * as S from "../styles"
+import { StyleSheet } from "react-native";
 
 export const PartoDomiciliar = () => {
-  <BackgroundComponent colors={["#FCCCD4", "#FCE5BF", "#E3C3B9"]}>
-    <View style={styles.body}></View>
-  </BackgroundComponent>;
+  return (
+    <BackgroundComponent colors={['#FCCCD4', '#FCE5BF', '#E3C3B9']}>
+      <S.Container>
+        <S.Form>
+          <TextComponent variant="Title1">
+
+            O parto domiciliar é quando uma gestante dá à luz em casa, em vez de um hospital.
+            Tem vantagens, como um ambiente familiar e menos intervenções médicas, mas também riscos,
+            pois complicações podem surgir sem recursos médicos imediatos. A decisão deve ser bem avaliada,
+            considerando a saúde da gestante, assistência qualificada e plano de contingência.
+            É importante verificar a legalidade e regulamentação locais.
+
+          </TextComponent>
+        </S.Form>
+      </S.Container>
+    </BackgroundComponent>
+  );
 };
 
 const styles = StyleSheet.create({
