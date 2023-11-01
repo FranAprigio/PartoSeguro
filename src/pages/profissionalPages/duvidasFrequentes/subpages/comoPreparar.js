f2d793b47c26bd54a63a0dd78521a61e953afa68
FranAprigio <francianefad@outlook.com>
2023-11-01 05:23:22 -0300

    [FEAT]: add duvidas frequentes prof

--- src/pages/profissionalPages/duvidasFrequentes/subpages/comoPreparar.js
+++ src/pages/profissionalPages/duvidasFrequentes/subpages/comoPreparar.js
@@ -1,13 +1,26 @@
-import {
-  BackgroundComponent,
-  TextComponent,
-} from "../../../../components/micros";
-import { StyleSheet, View } from "react-native";
+import React from 'react';
+import { BackgroundComponent, TextComponent } from "../../../../components/micros/index";
+import * as S from "../styles"
+import { StyleSheet } from "react-native";
 
 export const ComoPreparar = () => {
-  <BackgroundComponent colors={["#FCCCD4", "#FCE5BF", "#E3C3B9"]}>
-    <View style={styles.body}></View>
-  </BackgroundComponent>;
+  return (
+    <BackgroundComponent colors={['#FCCCD4', '#FCE5BF', '#E3C3B9']}>
+      <S.Container>
+        <S.Form>
+          <TextComponent variant="Title1">
+
+            Conhecimento da Legislação e Diretrizes Éticas: Esteja ciente das leis e
+            regulamentos que regem a prática obstétrica em sua região, bem como diretrizes éticas relacionadas ao consentimento informado, privacidade e confidencialidade.
+
+            Equipamento e Suprimentos: Certifique-se de ter acesso ao equipamento e aos
+            suprimentos necessários para atender às necessidades da gestante e do bebê durante o parto.
+
+          </TextComponent>
+        </S.Form>
+      </S.Container>
+    </BackgroundComponent>
+  );
 };
 
 const styles = StyleSheet.create({
--- src/pages/profissionalPages/duvidasFrequentes/subpages/diferencaParto.js
+++ src/pages/profissionalPages/duvidasFrequentes/subpages/diferencaParto.js
@@ -1,13 +1,33 @@
-import {
-  BackgroundComponent,
-  TextComponent,
-} from "../../../../components/micros";
-import { StyleSheet, View } from "react-native";
+import React from 'react';
+import { BackgroundComponent, TextComponent } from "../../../../components/micros/index";
+import * as S from "../styles"
+import { StyleSheet } from "react-native";
 
 export const DiferencaParto = () => {
-  <BackgroundComponent colors={["#FCCCD4", "#FCE5BF", "#E3C3B9"]}>
-    <View style={styles.body}></View>
-  </BackgroundComponent>;
+  return (
+    <BackgroundComponent colors={['#FCCCD4', '#FCE5BF', '#E3C3B9']}>
+      <S.Container>
+        <S.Form>
+          <TextComponent variant="Title1">
+
+            Parto Vaginal Normal (PVN):
+
+            Ocorre através do canal vaginal.
+            Pode ser espontâneo ou induzido.
+            Geralmente, não requer intervenções médicas significativas.
+            A recuperação costuma ser mais rápida comparada a uma cesariana.
+            Cesariana:
+
+            Envolve uma incisão cirúrgica no abdômen e no útero para a retirada do bebê.
+            É geralmente programada ou realizada em situações de emergência.
+            Pode ser necessária devido a complicações médicas, posição do feto, partos múltiplos, entre outros motivos.
+            A recuperação costuma ser mais demorada e pode envolver mais desconforto.
+
+          </TextComponent>
+        </S.Form>
+      </S.Container>
+    </BackgroundComponent>
+  );
 };
 
 const styles = StyleSheet.create({
